4c33b5e882b042a773626cdc3d7ded7128bad029
Alex Withington-Smith <alex@awithsmith.com>
2020-11-01 07:50:59 +0000

    added #dock initial test and made it pass

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,4 +1,5 @@
 require 'dockingstation'
+require 'bike'
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
@@ -8,6 +9,15 @@ describe DockingStation do
       bike = DockingStation.new.release_bike
       expect(bike).to be_a_kind_of(Bike)
       expect(bike.working?).to eq(true)
+    end
+  end
+
+  describe "#dock" do
+    it "adds a bike to the docking station" do
+      bike = Bike.new
+      station = DockingStation.new
+      station.dock(bike)
+      expect(station.bikes).to include(bike) 
     end
   end
 end
