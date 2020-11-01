3ec50f93562dfe2f02f477e8235edf405009ca1d
Alex Withington-Smith <alex@awithsmith.com>
2020-11-01 07:47:07 +0000

    added test to see if #release_bike releases a working bike

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -2,4 +2,12 @@ require 'dockingstation'
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
+
+  describe "#release_bike" do
+    it "releases a working bike" do
+      bike = DockingStation.new.release_bike
+      expect(bike).to be_a_kind_of(Bike)
+      expect(bike.working?).to eq(true)
+    end
+  end
 end
