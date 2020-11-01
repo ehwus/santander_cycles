a04e3db0d3e7d6c7629d72a88e1468ec92d86e02
Alex Withington-Smith <alex@awithsmith.com>
2020-11-01 09:52:47 +0000

    added the functionality for a user to declare a bike not working

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -2,4 +2,13 @@ require 'bike'
 
 describe Bike do
   it { is_expected.to respond_to :working? }
+
+  describe "#report_broken" do
+    it "changes working? status from true to false" do
+      bike = Bike.new
+      expect(bike.working?).to be(true)
+      bike.report_broken
+      expect(bike.working?).to be(false)
+    end
+  end
 end
